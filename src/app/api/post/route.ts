d1725aabbbca83278e3d725a9f056cfecc5f72f5
root <root@pop-os.localdomain>
2023-10-23 12:36:32 +0300

    Basic Common Filters

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -1,6 +1,26 @@
 import prisma from "@/lib/prisma";
 
 export async function GET(){
-    const posts = await prisma.post.findMany();
+    const posts = await prisma.post.findMany({
+        where: {
+            OR:[
+                {
+                    title: {
+                        contains: "Twitter",
+                        mode: "insensitive"
+                    }
+                },
+                {
+                    title: {
+                        contains: "github",
+                        mode: "insensitive"
+                    }
+                }
+            ],
+            AND: {
+                published: true
+            }
+        }
+    });
     return new Response(JSON.stringify(posts));
 }
--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,15 +1,19 @@
 import prisma from "@/lib/prisma";
 
 export async function GET(request: Request) {
-    const skip  = 1;
+    let searchTerm: string = "sAM".toLowerCase();
 
-    const users = await prisma.user.findMany({
-        orderBy: {
-            id: 'asc',
-        },
-        skip,
-        take: 2,
-    });
+    const users = await prisma.user.findMany(
+    {
+        where: {
+            id: {
+                not: {
+                    in: [1, 2]
+                }
+            }
+        }
+    }
+    );
 
     return new Response(JSON.stringify(users));
 }
