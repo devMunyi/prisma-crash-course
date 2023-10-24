b614c1f1d60ebeaed00734546a5437174d38d44d
root <root@pop-os.localdomain>
2023-10-24 22:39:48 +0300

    Aggregation, pagination, ordering, grouping

--- src/app/api/post/pagination/route.ts
+++ src/app/api/post/pagination/route.ts
@@ -25,7 +25,13 @@ export async function GET(request: Request) {
                         } 
                     }
                 }
-            ]
+            ],
+            AND: {
+                published: false,
+                id: {
+                    gt: 0
+                }
+            }
         },
         orderBy: {
             id: 'asc'
